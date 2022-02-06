3213e1bcf79cc03c8baa95067b321ef621d18687
Radar2411 <75230037+Radar2411@users.noreply.github.com>
2022-02-07 00:56:11 +0800

    add addExercise and showOwnExercise page

--- lib/pages/addExercise.dart
+++ lib/pages/addExercise.dart
@@ -1,17 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:freestyle_exercise/services/firestore_service.dart';
 
-class AddExercise extends StatelessWidget {
-  const AddExercise({Key key}) : super(key: key);
+import 'package:fluttertoast/fluttertoast.dart';
+
+class AddExercise extends StatefulWidget {
+  @override
+  _AddExerciseState createState() => _AddExerciseState();
+}
+
+class _AddExerciseState extends State<AddExercise> {
+  String exerciseName;
+  String exerciseDuration;
+  String numberOfReps;
+
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Icon(
-        Icons.run_circle,
-        size: 120.0,
-        color: Colors.orange,
+      appBar: AppBar(title: Text('Adding Own Exercise'), actions: <Widget>[]),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(labelText: 'Exercise Name'),
+                  validator: (val) =>
+                      val.length == 0 ? "Enter Exercise Name" : null,
+                  onSaved: (val) => this.exerciseName = val,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(labelText: 'Exercise Duration'),
+                  validator: (val) =>
+                      val.length == 0 ? 'Enter Exercise Duration' : null,
+                  onSaved: (val) => this.exerciseDuration = val,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(labelText: 'Number Of Reps'),
+                  validator: (val) =>
+                      val.length == 0 ? 'Enter Number Of Reps' : null,
+                  onSaved: (val) => this.numberOfReps = val,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 10.0),
+                  child: RaisedButton(
+                    onPressed: _submit,
+                    child: Text('Add Exercise'),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
-    ));
+    );
   }
-}
+
+  void _submit() {
+    if (this.formKey.currentState.validate()) {
+      formKey.currentState.save();
+    } else {
+      return null;
+    }
+    FirestoreService()
+        .addExerciseData(exerciseName, exerciseDuration, numberOfReps);
+    Fluttertoast.showToast(
+        msg: "Data saved successfully", gravity: ToastGravity.TOP);
+  } //_submit
+} //_AddRecordPageState
---
+++ lib/pages/showOwnExercise.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:freestyle_exercise/model/ownExercise.dart';
+
+import 'package:freestyle_exercise/services/firestore_service.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class RecordsPage extends StatefulWidget {
+  @override
+  _RecordsPageState createState() => _RecordsPageState();
+}
+
+class _RecordsPageState extends State<RecordsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Own Exercises'),
+      ),
+      body: FutureBuilder<List<OwnExercise>>(
+        future: FirestoreService().readOwnExerciseData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 5,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        snapshot.data[index].exerciseTitle,
+                        style: TextStyle(color: Colors.blue, fontSize: 16.0),
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            snapshot.data[index].duration,
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 14.0),
+                          ),
+                          Text(
+                            snapshot.data[index].noOfReps,
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 12.0),
+                          ),
+                        ],
+                      ),
+                      IconButton(
+                        color: Colors.blue,
+                        icon: Icon(Icons.delete),
+                        onPressed: () async {
+                          setState(() {
+                            FirestoreService().deleteOwnExerciseData(
+                                snapshot.data[index].uid);
+                          });
+                          Fluttertoast.showToast(
+                              msg: "Data deleted successfully",
+                              gravity: ToastGravity.TOP);
+                        },
+                      )
+                    ],
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+          return Container(
+            alignment: AlignmentDirectional.center,
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+} //_RecordsPageState

30562da6356da37a1f0e400b484d83e127c4f3a5
Radar2411 <75230037+Radar2411@users.noreply.github.com>
2022-02-07 21:42:03 +0800

    do welcome page

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -1,17 +1,30 @@
+import 'package:freestyle_exercise/widgets/exercises_widget.dart';
+import 'package:freestyle_exercise/widgets/line_chart_widget.dart';
 import 'package:flutter/material.dart';
 
 class Welcome extends StatelessWidget {
-  const Welcome({Key key}) : super(key: key);
-
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Icon(
-        Icons.home,
-        size: 120.0,
-        color: Colors.orange,
-      ),
-    ));
-  }
+  Widget build(BuildContext context) => Scaffold(
+        body: CustomScrollView(
+          physics: BouncingScrollPhysics(),
+          slivers: [
+            buildAppBar(context),
+            ExercisesWidget(),
+          ],
+        ),
+      );
+
+  SliverAppBar buildAppBar(BuildContext context) => SliverAppBar(
+        flexibleSpace: FlexibleSpaceBar(background: LineChartWidget()),
+        expandedHeight: MediaQuery.of(context).size.height * 0.5,
+        stretch: true,
+        title: Text('Statistics'),
+        centerTitle: true,
+        pinned: true,
+        leading: Icon(Icons.menu),
+        actions: [
+          Icon(Icons.person, size: 28),
+          SizedBox(width: 12),
+        ],
+      );
 }
